257ea1b27f240762c3d933cdc415a120ed4c5ed4
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-04-04 22:46:31 -0400

    Need to familiarize myself better with file IO. It's extremely useful for data analysis. all tests passed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a", encoding="utf-8") as appendFile:
+        appendFile.write(append_content)
 
 def read_file(file_name):
-    pass
+    fileRead = open(f"{file_name}.txt", encoding="utf-8")
+    return fileRead.read()
+
+file_name = "test_file"
+file_content = "This is a test content."
+write_file(file_name, file_content)
+file_content_read = read_file(file_name)
+print(file_content_read)
